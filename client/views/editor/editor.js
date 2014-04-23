3c3b3f8e1deecad519590bfad2751923683e2cf3
denkhaus <peristaltic@gmx.net>
2014-04-24 00:26:52 +0200

    started saving function

--- client/views/editor/editor.js
+++ client/views/editor/editor.js
@@ -1,30 +1,60 @@
-Template.editor.rendered = function(){
+Template.editor.rendered = function () {
 
-    var elm = this.find("#aceEditor");
-    var newId = "aceEdit-" + this.data.id;
-    elm.id = newId;
+    var elmEdit = this.find("#aceEditor");
+    var newEditorId = "aceEdit-" + this.data.id;
+    elmEdit.id = newEditorId
 
-    var editor = ace.edit(newId);
+    var elmStatus = this.find("#aceStatusBar");
+    elmStatus.id = "aceStatus-" + this.data.id;
+
+    var editor = ace.edit(newEditorId);
     //editor.setTheme("ace/theme/monokai");
     //editor.getSession().setMode("ace/mode/css");
     //editor.setHighlightActiveLine(true);
+
     editor.setAutoScrollEditorIntoView();
     registerEditor(this.data.id, editor);
 
-    $(elm).css({
+    editor.getSession().on('change', function (e) {
+
+    });
+
+    //var StatusBar = ace.require('ace/ext/statusbar').StatusBar;
+    //var statusBar = new StatusBar(editor, elmStatus);
+
+    $(elmEdit).css({
         position: "absolute",
-        top: 42,
+        top: 40,
         right: 0,
         bottom: 0,
         left: 0
     });
 
-    var path = this.data.path;
-    Meteor.call('editorGetFileContent', path, function(err, res){
-        if(!err){
-            editor.setValue(res)
-        } else{
-            console.log("Can't get file content of path " + path + " Error:: " + err);
+    editor.commands.addCommand({
+        name: "showKeyboardShortcuts",
+        bindKey: {
+            win: "Ctrl-Alt-h",
+            mac: "Command-Alt-h"
+        },
+        exec: function (editor) {
+            ace.config.loadModule("ace/ext/keybinding_menu", function (module) {
+                module.init(editor);
+                editor.showKeyboardShortcuts()
+            })
         }
     });
+
+    editor.commands.addCommand({
+        name: 'safeContent',
+        bindKey: {
+            win: 'Ctrl-S',
+            mac: 'Command-S'
+        },
+        exec: function (editor) {
+            editorSaveContent(editor, this.data.path);
+        },
+        readOnly: false // false if this command should not apply in readOnly mode
+    });
+
+    editorLoadContent(editor, this.data.path);
 }
--- client/views/main/main.js
+++ client/views/main/main.js
@@ -1,11 +1,16 @@
 
 Template.main.rendered = function () {
-    $('#lmain').layout({ applyDefaultStyles: true , west:{minSize:300 }});
-    $('#lcenter').layout({applyDefaultStyles: true,
-        south:{size:200, resizable:true},
-        onresize: function(name, elm, state, options, layout){
-            if(name == "center"){
-                resizeEditorsToFit(state.innerWidth, state.innerHeight);
-            }
-    }});
+    $('#lmain').layout({ 
+        applyDefaultStyles: true , 
+        west:{ minSize: 300, size: "auto", closable:true, resizable:true, slidable:true, livePaneResizing: true}
+    });
+    
+    $('#lcenter').layout({ 
+        applyDefaultStyles: true,
+        south:{ minSize: 300, size:"auto", closable:true, resizable:true, slidable:true, livePaneResizing: true}
+        //,onresize: function(name, elm, state, options, layout){
+        //    if(name == "center"){
+        //        resizeEditorsToFit(state.innerWidth, state.innerHeight);
+        //    }}
+    });
 };
